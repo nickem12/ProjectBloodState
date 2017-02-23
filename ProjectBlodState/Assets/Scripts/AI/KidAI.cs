ff8cb871c45956f974a76ccb7580f7cf8d4de13d
Nick Doucette <nidoucette@hotmail.com>
2017-02-23 12:04:40 -0400

    pathfinding updated

---
+++ ProjectBlodState/Assets/Scripts/AI/KidAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidAI : MonoBehaviour {
+
+    enum State {IDLE, MOVING, MOVESELECT}
+    State state;
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = State.IDLE;
+	}
+	
+    void Move()
+    {
+
+    }
+
+    void SetMove()
+    {
+        this.GetComponent<FindPath>().GetPath(this.transform.position, GetRayCast("Terrain"));
+    }
+
+    Vector3 GetRayCast(string input) //returns tag of selected object
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            if(hit.collider.tag == input)
+            {
+                return (hit.transform.position);
+            }
+            else
+            {
+                return (new Vector3(0,0,0));
+            }
+        }
+        else
+        {
+            return (new Vector3(0, 0, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        switch(state)
+        {
+            case State.IDLE:
+
+                break;
+
+            case State.MOVING:
+                Move();
+                break;
+        }
+	}
+}
---
+++ ProjectBlodState/Assets/Scripts/General/FindPath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPath : MonoBehaviour {
+
+    TGS.TerrainGridSystem tgs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        tgs = TGS.TerrainGridSystem.instance;
+	}
+
+    public List<int> GetPath(Vector3 charPos, Vector3 targetPos)
+    {
+        int startCell = tgs.CellGetIndex(tgs.CellGetAtPosition(charPos));
+        int endCell = tgs.CellGetIndex(tgs.CellGetAtPosition(targetPos));
+
+        List<int> PathList = tgs.FindPath(startCell, endCell, 0);
+        return (PathList);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
