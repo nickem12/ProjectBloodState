d145c8817adbcfd5b655e64c964742429e6ef29b
Benjamin08 <RFLXcolby@gmail.com>
2017-03-28 17:46:18 -0300

    Animation Helper Script
    Added a animation helper script and set up some of the blend tree
    animations. BIg Guy still doesn have any animations

---
+++ ProjectBlodState/Assets/Scripts/Animation Helper Script/HelperAnimatiorScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperAnimatiorScript : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void Animator_SetSpeed(float speed)
+    {
+        anim.SetFloat("Speed", speed);
+    }
+    
+    public void Animator_SetSquat(bool value)
+    {
+        anim.SetBool("Squat", value);
+    }
+
+    public void Animator_SetAiming(bool value)
+    {
+        anim.SetBool("Aiming", value);
+    }
+
+    public void Animator_Attack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    public void Animator_Jump()
+    {
+        anim.SetTrigger("Jump");
+    }
+
+    public void Animator_Damage()
+    {
+        anim.SetTrigger("Damage");
+    }
+
+    public void Animator_SetDamageID(int DamageID)
+    {
+        anim.SetInteger("DamageID", DamageID);
+    }
+
+    public void Animator_Death()
+    {
+        anim.SetTrigger("Death");
+    }
+}
